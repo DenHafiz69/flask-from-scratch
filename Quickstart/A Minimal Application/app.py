c20ae7ff2b5fe64cd671e02c153d384050fc20bc
Den Hafiz <den.m.hafiz@gmail.com>
2023-03-24 07:52:32 +0800

    Escape function

--- Quickstart/A Minimal Application/app.py
+++ Quickstart/A Minimal Application/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__) # creates an instance of Flask class
 
 @app.route("/") # tell Flask which URL to trigger the function below
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h1>Hello, World!</h1>"
+
+@app.route("/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}!"
