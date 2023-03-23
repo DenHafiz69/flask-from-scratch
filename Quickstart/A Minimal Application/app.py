003d586fb0c6ea50748c25afd99282fa207b12da
Den Hafiz <den.m.hafiz@gmail.com>
2023-03-24 07:40:42 +0800

    Added some comments

--- Quickstart/A Minimal Application/app.py
+++ Quickstart/A Minimal Application/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) # creates an instance of Flask class
 
-@app.route("/")
+@app.route("/") # tell Flask which URL to trigger the function below
 def hello_world():
     return "<p>Hello, World!</p>"
